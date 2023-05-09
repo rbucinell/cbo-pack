2f393ac379bc8f1cb21e1bbf1e09b003924edfca
Bucinell, Ryan <rbucinell.dev@gmail.com>
2023-05-09 01:37:42 -0400

    coding standards

--- src/attendence_from_logs.js
+++ src/attendence_from_logs.js
@@ -7,7 +7,13 @@ import { rmSync } from 'fs';
 
 const log_dir = `${process.env.USERPROFILE}\\Documents\\Guild Wars 2\\addons\\arcdps\\arcdps.cbtlogs\\WvW\\`;
 
-const todaysZEVTC = async ( executeDate ) => {
+/**
+ * Finds all zevtc files that were generated on a given date
+ * 
+ * @param {Date} executeDate the date of the logs we are looking for
+ * @returns Array<string> list of zevtc file names
+ */
+const zevtcForDate = async ( executeDate ) => {
     let date = new Date(executeDate);
     date.setMinutes( date.getMinutes() - date.getTimezoneOffset() );
     const dateStr = date.toISOString().slice(0,10);
@@ -15,6 +21,13 @@ const todaysZEVTC = async ( executeDate ) => {
     return (await readdir(log_dir)).filter( f => path.basename(f).startsWith(fileDateStr) && f.endsWith('.zevtc'));
 }
 
+/**
+ * looks for the JSON equivalent of given zevtc file. If missing it will use parser to generate one.
+ * (Deletes generated log file to keep dir clean).
+ * 
+ * @param {*} zevtcPath path to zevtc file
+ * @returns zevtc's JSON genrated file path
+ */
 const zevtcToJSON = async ( zevtcPath ) => {
     const jsonPath = zevtcPath.replace('.zevtc', '_wvw_kill.json');
     try {    
@@ -27,12 +40,24 @@ const zevtcToJSON = async ( zevtcPath ) => {
     return jsonPath;
 }
 
-const readData = async ( f ) => {
+/**
+ * extracts data from zevtc file in JSON format.
+ * 
+ * @param {string} f file path of zevtc file
+ * @returns object representing data in file
+ */
+const readZevtcJSONData = async ( f ) => {
     let jsonPath = await zevtcToJSON(log_dir + f);
     let fileBuffer = await readFile( jsonPath );
     return JSON.parse( fileBuffer );
 }
 
+/**
+ * Logs attendence data to screen in markdown format. (For copy-pasting into discord).
+ * 
+ * @param {Date} executeDate the date of the logs we are looking for
+ * @param {Array} attendence list of player data (account and names) that were in attendence on given date
+ */
 const reportAttendence = ( executeDate, attendence ) => {
     console.log( `-----------------------------------\nAccording to the logs, tonight's (${executeDate.toDateString()}) attendence:\n\`\`\``);
     let longestAcct = Math.max(...attendence.map(a => a.account.length));
@@ -44,15 +69,19 @@ const reportAttendence = ( executeDate, attendence ) => {
     console.log( '\`\`\`');
 }
 
-const main = async ( executeDate ) => {
+/**
+ * Main function. Takes one argument, given date, otherwise uses current date.
+ */
+(async () => {
+    let executeDate = process.argv.length > 2 ? new Date(args[2]) : new Date();
     console.log(`Parsing logs for ${executeDate.toDateString()}`);
-    let files = await todaysZEVTC(executeDate);
+    let files = await zevtcForDate(executeDate);
     let attendence = [];
 
     let i = 1;
     for( let f of files ) {
         console.log( `[${i++}/${files.length}] ${f}` );
-        let players = (await readData( f )).players;
+        let players = (await readZevtcJSONData( f )).players;
         players.filter( p => !p.notInSquad ).forEach( player => {
             if( !attendence.find( p => p.account === player.account))
                 attendence.push(player);
@@ -61,7 +90,4 @@ const main = async ( executeDate ) => {
     }
     attendence.sort((a, b) => a.account.toLowerCase().localeCompare(b.account.toLowerCase()));
     reportAttendence( executeDate, attendence );
-}
-let args = process.argv.slice(2);
-let today = args.length > 0 ? new Date(args[0]) : new Date();
-await main(today);
+})();
